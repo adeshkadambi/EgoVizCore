31148a93bfff97fc049ad8b6344625923226efeb
Adesh <adeshkadambi@gmail.com>
2022-05-29 14:24:32 -0400

    test build

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function App() {
   return (
+    <div>
       <h1 className="text-slate-50 text-2xl font-semibold flex justify-center align-middle mt-[25%]">
         Coming Soon
       </h1>
+      <p className="flex justify-center text-slate-400">CDSS Study Web App lives here.</p>
+    </div>
   );
 }
 
