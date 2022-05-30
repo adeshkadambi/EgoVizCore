d552098fdec694d5acfd0d1e7491ba4f70d10c42
Adesh <adeshkadambi@gmail.com>
2022-05-30 01:07:20 -0400

    firebase auth cleanup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
       navigate('/dashboard')
     } catch (e) {
       setError(e.message)
-      console.log(e.message)
+      console.log(e.message, error)
     }
   };
 
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,38 +1,34 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase';
 
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState({});
+    const [user, setUser] = useState({});
 
-  const createUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
-
-   const signIn = (email, password) =>  {
+    const signIn = (email, password) =>  {
     return signInWithEmailAndPassword(auth, email, password)
-   }
+    }
 
-  const logout = () => {
-      return signOut(auth)
-  }
+    const logout = () => {
+        return signOut(auth)
+    }
 
-  useEffect(() => {
+    useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
+        setUser(currentUser);
     });
     return () => {
-      unsubscribe();
+        unsubscribe();
     };
-  }, []);
+    }, []);
 
-  return (
+    return (
     <UserContext.Provider value={{ user, logout, signIn }}>
-      {children}
+        {children}
     </UserContext.Provider>
-  );
+    );
 };
 
 export const UserAuth = () => {
