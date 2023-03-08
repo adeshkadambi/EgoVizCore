4d5f4afcb29556b5b37bfbbe204a18188a59345f
Adesh <adeshkadambi@gmail.com>
2023-03-07 23:58:16 -0500

    p-03 videos

--- src/data/videodata.js
+++ src/data/videodata.js
@@ -1,42 +1,65 @@
+// export const videos = [
+//     {
+//         name: 'Cleaning',
+//         duration: '00:29',
+//         url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/cleaning.mp4?alt=media&token=752ddd88-b327-4e04-9d81-204630e2b23b',
+//     },
+//     {
+//         name: 'Eating 1',
+//         duration: '00:27',
+//         url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/eating1.mp4?alt=media&token=76afcbac-588c-4115-a081-228f47f60ca2',
+//     },
+//     {
+//         name: 'Eating 2',
+//         duration: '00:50',
+//         url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/eating2.mp4?alt=media&token=4c8681e3-f9ba-45ed-aa3e-492bdc555e83',
+//     },
+//     {
+//         name: 'Groceries',
+//         duration: '00:46',
+//         url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/groceries.mp4?alt=media&token=ad9011c8-a878-4359-bab3-2411dd79e42c',
+//     },
+//     {
+//         name: 'Knitting',
+//         duration: '00:26',
+//         url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/knitting.mp4?alt=media&token=4d9e44a7-660d-41a9-94b0-49c253b0bbbb',
+//     },
+//     {
+//         name: 'Pills',
+//         duration: '00:41',
+//         url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/pills.mp4?alt=media&token=ed54e792-8ae9-4545-b96c-edb32143c698',
+//     },
+//     {
+//         name: 'Typing',
+//         duration: '00:39',
+//         url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/typing.mp4?alt=media&token=723ba4f8-2202-42f6-a9d2-7fe65a6ab7b7',
+//     },
+//     {
+//         name: 'Writing',
+//         duration: '00:34',
+//         url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/writing.mp4?alt=media&token=51eff6b0-55a7-4caa-b240-55f5a03149b6',
+//     },
+// ];
+
 export const videos = [
-    {
-        name: 'Cleaning',
-        duration: '00:29',
-        url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/cleaning.mp4?alt=media&token=752ddd88-b327-4e04-9d81-204630e2b23b',
-    },
-    {
-        name: 'Eating 1',
-        duration: '00:27',
-        url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/eating1.mp4?alt=media&token=76afcbac-588c-4115-a081-228f47f60ca2',
-    },
-    {
-        name: 'Eating 2',
-        duration: '00:50',
-        url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/eating2.mp4?alt=media&token=4c8681e3-f9ba-45ed-aa3e-492bdc555e83',
-    },
-    {
-        name: 'Groceries',
-        duration: '00:46',
-        url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/groceries.mp4?alt=media&token=ad9011c8-a878-4359-bab3-2411dd79e42c',
-    },
-    {
-        name: 'Knitting',
-        duration: '00:26',
-        url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/knitting.mp4?alt=media&token=4d9e44a7-660d-41a9-94b0-49c253b0bbbb',
-    },
-    {
-        name: 'Pills',
-        duration: '00:41',
-        url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/pills.mp4?alt=media&token=ed54e792-8ae9-4545-b96c-edb32143c698',
-    },
-    {
-        name: 'Typing',
-        duration: '00:39',
-        url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/typing.mp4?alt=media&token=723ba4f8-2202-42f6-a9d2-7fe65a6ab7b7',
-    },
-    {
-        name: 'Writing',
-        duration: '00:34',
-        url: 'https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/writing.mp4?alt=media&token=51eff6b0-55a7-4caa-b240-55f5a03149b6',
-    },
+  {
+    name: "Meal Prep 1",
+    duration: "01:00",
+    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-03%2FGOPR0501--1.MP4?alt=media&token=eb82c5f4-55b9-4f84-a15c-3b41ab6773d6",
+  },
+  {
+    name: "Meal Prep 2",
+    duration: "01:00",
+    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-03%2FGOPR0507--1.MP4?alt=media&token=061c13fc-380c-487b-9ac8-4e805f4ecea4",
+  },
+  {
+    name: "Functional Mobility 1",
+    duration: "01:00",
+    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-03%2FGOPR0516--2.MP4?alt=media&token=8a1af390-cd1f-415c-bcee-0cabdd0a8538",
+  },
+  {
+    name: "Leisure 1",
+    duration: "01:00",
+    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-03%2FGOPR0777--7.MP4?alt=media&token=adfc5d31-3eac-440e-a5ea-d3aa0d7ec69c",
+  },
 ];
