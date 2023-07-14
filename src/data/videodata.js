5c1da5df3e03d34efd9747687da8a37cc831d0ec
Adesh <adeshkadambi@gmail.com>
2023-07-14 02:32:40 -0400

    p-04

--- src/data/videodata.js
+++ src/data/videodata.js
@@ -41,6 +41,7 @@
 //     },
 // ];
 
+// // P-03
 // export const videos = [
 //   {
 //     name: "Meal Prep 1",
@@ -64,27 +65,52 @@
 //   },
 // ];
 
-// P-02
+// // P-02
+
+// export const videos = [
+//   {
+//     name: "Meal Prep 1",
+//     duration: "01:00",
+//     url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-02%2FGP012790--2.MP4?alt=media&token=dcd0221e-7125-4546-8c43-da3341042958",
+//   },
+//   {
+//     name: "Feeding 1",
+//     duration: "01:00",
+//     url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-02%2FGP062788--5.MP4?alt=media&token=b5efa7e2-47b9-4e44-9276-68bf624a5fea",
+//   },
+//   {
+//     name: "Home Mangement 1",
+//     duration: "01:00",
+//     url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-02%2FGP012789--7.MP4?alt=media&token=dc8d6693-f315-42f5-8b8e-31159b771711",
+//   },
+//   {
+//     name: "Communication Management 1",
+//     duration: "01:00",
+//     url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-02%2FGP012788--2.MP4?alt=media&token=1b45351c-939f-4d2e-826e-d1d359d6a368",
+//   },
+// ];
+
+// P-04
 
 export const videos = [
   {
     name: "Meal Prep 1",
     duration: "01:00",
-    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-02%2FGP012790--2.MP4?alt=media&token=dcd0221e-7125-4546-8c43-da3341042958",
+    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-04%2FGOPR1125--7.MP4?alt=media&token=e43d7541-2492-4d54-902a-5151880e3fd5",
   },
   {
-    name: "Feeding 1",
+    name: "Health Management 1",
     duration: "01:00",
-    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-02%2FGP062788--5.MP4?alt=media&token=b5efa7e2-47b9-4e44-9276-68bf624a5fea",
+    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-04%2FGOPR1055--4.MP4?alt=media&token=953c3891-93a1-460f-945f-a1bf8eef07e7",
   },
   {
     name: "Home Mangement 1",
     duration: "01:00",
-    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-02%2FGP012789--7.MP4?alt=media&token=dc8d6693-f315-42f5-8b8e-31159b771711",
+    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-04%2FGOPR1130--11.MP4?alt=media&token=b5aebcd6-4227-4a3d-815c-fb3da9f85a4b",
   },
   {
-    name: "Communication Management 1",
+    name: "Home Mangement 2",
     duration: "01:00",
-    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-02%2FGP012788--2.MP4?alt=media&token=1b45351c-939f-4d2e-826e-d1d359d6a368",
+    url: "https://firebasestorage.googleapis.com/v0/b/egocdss.appspot.com/o/p-04%2FGP010880--2.MP4?alt=media&token=ba5eb589-6e15-4353-a580-2844b995267b",
   },
 ];
